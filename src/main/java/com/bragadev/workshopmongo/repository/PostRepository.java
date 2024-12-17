831101716cf9a49bf0179759e9a171f6660cd038
alexbragadev <alexbraga107@gmail.com>
2024-12-17 16:10:05 -0300

    query com parametros para pesquisa com datas

--- src/main/java/com/bragadev/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/bragadev/workshopmongo/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package com.bragadev.workshopmongo.repository;
 
-import java.util.List;
-
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,4 +14,6 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	@Query("{'title': {$regex: ?0, $options: 'i'}}")
 	List<Post> findByTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{ $and: [{date: {$gte: ?1}},{date: {$lte: ?2}},{$or: [{'title': {$regex: ?0, $options: 'i'}},{'body': {$regex: ?0, $options: 'i'}},{'comments.text': {$regex: ?0, $options: 'i'}}]}]}")
+	List<Post> fullsearch(String text, Date minDate, Date maxDate);
 }
